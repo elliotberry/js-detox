77a1eb6c72a2851fd3001f48166fa6ab10d06c62
Elliot Berry <elliot@hotmail.wf>
2019-11-03 11:38:34 -0500

    a lot of script additions

--- app.js
+++ app.js
@@ -1,112 +1,72 @@
- const fs = require('fs');
- const path = require('path');
- const chalk = require('chalk');
- const prettyBytes = require('pretty-bytes');
- var prompt = require('prompt-sync')();
- const yn = require('yn');
-
- const {
-   exec
- } = require('child_process');
-
- function getFilesize(filename) {
-   const stats = fs.statSync(filename);
-   const fileSizeInBytes = stats.size;
-   return prettyBytes(fileSizeInBytes);
- }
-
- function fd() {
-   return new Promise(function (res, rej) {
-     exec('fd . -t f -H', (err, stdout, stderr) => {
-       if (err) {
-         // node couldn't execute the command
-         return;
-       }
-       let y = stdout.split('\n');
-       var filtered = y.filter(function (el) {
-         return el != '';
-       });
-       res(filtered);
-     });
-
-   });
- }
-
- function replaceAll(str, find, replace) {
-   return str.replace(new RegExp(find, 'g'), replace);
- }
-
- function headCat(file) {
-   return new Promise(function (res, rej) {
-     exec('cat ' + file, (err, stdout, stderr) => {
-       if (err) {
-         // node couldn't execute the command
-         return;
-       }
-
-       let length = stdout.length;
-       let y = stdout.substring(0, 200);
-       let head = replaceAll(y, "\n", " ").trim();
-
-       let obj = {
-         path: file,
-         name: path.parse(file).base,
-         head: head,
-         length: length,
-         size: getFilesize(file)
-       }
-
-       res(obj);
-     });
-   });
- }
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+var program = require('commander');
+const slugify = require('slugify');
+const rename = require('rename');
+const thisDir = path.resolve('./');
+const {
+  exec
+} = require('child_process');
 
 
- function printTemplate(obj) {
 
 
-   console.log('###################################################');
-   console.log(chalk.blue.bold.underline(`Name:`), chalk.blue.bold(`${obj.name}`));
-   console.log(chalk.bold.green.underline(`File Head:`), chalk.bold.green(`"${obj.head}"`));
-   console.log(chalk.magenta.bold.underline(`Length:`), chalk.magenta.bold(`${obj.length} characters`));
-   console.log(chalk.cyan.bold.underline(`Size:`), chalk.cyan.bold(` ${obj.size}`));
-   console.log('###################################################');
+//process fd
+function fd() {
+  return new Promise(function (res, rej) {
+    
+    exec('fd --full-path ' + thisDir, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        return;
+      }
+      let y = stdout.split('\n');
+      var filtered = y.filter(function (el) {
+        return el != '';
+      });
+      res(filtered);
+    });
+  });
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
+
+let format = function (name) {
+  let y = slugify(name);
+  return replaceAll(y, "_", "-");
+}
+
+function printTemplate(obj) {
+
+  console.log(chalk.blue.bold.underline(`Name:`), chalk.blue.bold(`${obj.name}`));
+}
+
+async function start() {
+  
+  
+  let r = await fd();
+  r.forEach(function (f) {
+   
+    let p = path.parse(f);
+
+    let newName = format(p.name);
+    console.log(f);
+    if (p.name !== newName) {
+     
+        console.log(p.base + " --> " + newName);
+
+      if (!program.dryrun) {
+        console.log(thisDir + "/" + f);
+        rename(thisDir + "/" + f, thisDir + "/" + newName);
+      }
+    }
+  });
+}
 
 
- }
-
- function deleteFile(f) {
-
-   fs.unlink(f.path, function (error) {
-     if (error) {
-       throw error;
-     }
-   });
-
- }
-
- async function start() {
-   let r = await fd();
-
-   let total = r.length;
-   let i = 0;
-   r.forEach(function (fi) {
-     headCat(fi).then(function (item) {
-       i++;
-       console.log("");
-       console.log("");
-       console.log("");
-       console.log("---------" + i + " / " + total + "---------");
-       printTemplate(item);
-       let n = prompt('Delete this file? (y/N)');
-       if (yn(n) == true) {
-         deleteFile(item);
-         console.log("deleted " + item.path);
-       }
-
-     });
-
-   });
-
- }
- start();
+program.option('-d, --dryrun', 'Dry run (show changes)');
+program.parse(process.argv);
+start();
