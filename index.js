c5cf5d6073d84c809648607cdb9ac992cef1534d
Elliot Berry <eberry@dailybreak.com>
2021-01-07 01:42:33 -0500

    fwfge

--- index.js
+++ index.js
@@ -4,40 +4,41 @@ const path = require("path");
 const chalk = require("chalk");
 const format = require("./slugify.js");
 const fd = require("./fs2.js");
-const ignore = require("./match-ignore")
+//const ignore = require("./match-ignore");
 const thisDir = path.resolve("./");
 
-const findFilesToChange = async function(filesArray) {
+const findFilesToChange = async function (filesArray) {
   let filesToChange = [];
- for await (f of filesArray) {
+  for await (f of filesArray) {
     let p = path.parse(f);
     let oldName = p.name;
     let newName = format(oldName);
     if (p.name !== newName) {
-      filesToChange.push({oldName: oldName, newName: newName})
+      filesToChange.push({ oldName: oldName, newName: newName });
     }
   }
-  return filesToChange
-}
+  return filesToChange;
+};
+
 async function showFileUpdates(pathObjArr) {
-  pathObjArr.forEach(obj => {
+  pathObjArr.forEach((obj) => {
     console.log(
       chalk.blue.bold.underline(obj.oldName) +
         " --> " +
         chalk.blue.bold(obj.newName)
     );
   });
-
 }
 
 async function start() {
-
-    let filesArray = await fd(thisDir);
-    let filtered = await ignore(filesArray);
-   const filteredNum = filesArray.length - filtered.length
-    const filesToChange = await findFilesToChange(filtered)
-    console.log(`${filesToChange.length} files to rename. ${filteredNum} filtered with default ignore.`)
-
+  let filesArray = await fd(thisDir);
+  //let filtered = await ignore(filesArray);
+  //const filteredNum = filesArray.length - filtered.length;
+  const filesToChange = await findFilesToChange(filesArray);
+  console.log(
+    `${filesToChange.length} files to rename: \n`
+  );
+  showFileUpdates(filesToChange)
 }
 
 //program.option("-d, --dry-run", "Dry run (show changes)");
